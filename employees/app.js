764683d71b260e9c8d03ad284268bf02dd645f2e
Nonamed <druizr07@gmail.com>
2025-01-27 21:58:53 +1100

    import customers.json and fix js module type

--- employees/app.js
+++ employees/app.js
@@ -1,7 +1,9 @@
-
 const $ = (el) => document.querySelector(el);
 
 const $$ = (el) => document.querySelectorAll(el);
+
+import customers from "./customers.json" with {type: "json"};
+
 
 const btn = $$("li");
 
